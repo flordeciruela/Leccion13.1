836f8538dffafb8d85ad03e4c9ede053ddec1d29
flordeciruela <wendy.reyessan@gmail.com>
2017-03-23 15:05:09 -0500

    indentacion finalizada

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,24 @@
 
 function mostrar() {
-var nombre = document.getElementById("nombre").value;
-var apellido = document.getElementById("apellido").value;
-var dni = document.getElementById("dni").value;
-var direccion = document.getElementById("direccion").value;
-var persona = document.getElementsByClassName("datos");
+	var nombre = document.getElementById("nombre").value;
+	var apellido = document.getElementById("apellido").value;
+	var dni = document.getElementById("dni").value;
+	var direccion = document.getElementById("direccion").value;
+	var persona = document.getElementsByClassName("datos");
 
-if (persona[0].value.length == 0 || persona[1].value.length == 0 || persona[2].value.length == 0 || persona[3].value.length == 0) {
-}else{
+		if (persona[0].value.length == 0 ||
+				persona[1].value.length == 0 ||
+				persona[2].value.length == 0 ||
+				persona[3].value.length == 0) {
+		}else{
 
-	document.getElementById("ficha").innerHTML=
-        "<ul>" +
-          "<li>Nombre: " + nombre + "</li>" +
-          "<li>Apellido: " + apellido + "</li>" +
-          "<li>DNI: " + dni + "</li>" +
-          "<li>Dirección: " + direccion + "</li>" +
-        "</ul>";
-	}
+				document.getElementById("ficha").innerHTML=
+				"<ul>" +
+				"<li>Nombre: " + nombre + "</li>" +
+				"<li>Apellido: " + apellido + "</li>" +
+				"<li>DNI: " + dni + "</li>" +
+				"<li>Dirección: " + direccion + "</li>" +
+				"</ul>";
+		}
+
 }
